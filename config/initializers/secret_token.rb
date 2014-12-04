9b4e8a17b661cc9f00b241c181810abee6970cf4
Jesse Cai <mojacc@gmail.com>
2014-12-04 10:01:22 +0800

    Create a rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodomvcRailsBackEnd::Application.config.secret_key_base = '1bd6342b8ce9aef59c51ad218451f9134c0d07010c8e37fce41696f1de040781ad2eb72bcbb95144b3bfcdf984959b0642e17ef19f22336f6c3636f00a2c148e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TodomvcRailsBackEnd::Application.config.session_store :cookie_store, key: '_todomvc-rails-back-end_session'
